76fc271c76de496778a369021ee99702920b6d40
MattRuetz <matt.b.ruetz@gmail.com>
2022-02-18 13:00:29 -0500

    working progress bar, able to skip around. Video restarts at end, and play button switches to a pause button as video is playing. TODO: volume and playback speed controls working

--- script.js
+++ script.js
@@ -1,11 +1,58 @@
-
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.querySelector('.volume-icon');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
 
+const showPlayIcon = () => {
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+}
 
+
+const togglePlay = () => {
+    if(video.paused) {
+        video.play()
+        playBtn.classList.replace('fa-play', 'fa-pause');
+        playBtn.setAttribute('title', 'Pause');
+    } else {
+        video.pause();
+        playBtn.classList.replace('fa-pause', 'fa-play');
+        playBtn.setAttribute('title', 'Play');
+    }
+}
 
 // Progress Bar ---------------------------------- //
 
+// Calculate display time values
+const displayTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds < 10 && (seconds = `0${seconds}`);
+
+    return `${minutes}:${seconds}`;
+}
+
+//Update progress as video plays
+const updateProgress = () => {
+    progressBar.style.width = `${(video.currentTime / video.duration)*100}%`
+    currentTime.textContent = `${displayTime(video.currentTime)} /`
+    duration.textContent = `${displayTime(video.duration)}`
+}
+
+const setProgress = (event) => {
+    
+    const newTime = event.offsetX / progressRange.offsetWidth;
+    progressBar.style.width = `${newTime}%`;
+    video.currentTime = newTime * video.duration;
+    updateProgress();
+}
 
 
 // Volume Controls --------------------------- //
@@ -19,3 +66,10 @@
 // Fullscreen ------------------------------- //
 
 
+// Event Listeners
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
+video.addEventListener('ended', showPlayIcon);
+progressRange.addEventListener('click', setProgress);
